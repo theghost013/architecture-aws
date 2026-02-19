423b36d38d297dd5bba055cce7cd37fd82a94d4c
leon.c <leon.cariou@supdevinci-edu.fr>
2026-02-19 12:02:04 +0100

    chore: Configure ESLint, update frontend libraries, and apply stylistic code formatting.

--- phase 5-6/web/app/config/config.js
+++ phase 5-6/web/app/config/config.js
@@ -6,12 +6,12 @@ let config = {
   APP_DB_NAME: ""
 }
 
-var AWS = require('aws-sdk');
+var AWS = require("aws-sdk")
 var client = new AWS.SecretsManager({
     region: "us-east-1"
-});
+})
 
-const secretName = "Mydbsecret";
+const secretName = "Mydbsecret"
 
 //let APP_DB_USER;
 
@@ -21,23 +21,23 @@ client.getSecretValue({SecretId: secretName}, function(err, data) {
       config.APP_DB_NAME = "STUDENTS"
       config.APP_DB_PASSWORD = "student12"
       config.APP_DB_USER = "nodeapp"
-      console.log('Secrets not found. Proceeding with default values..')
+      console.log("Secrets not found. Proceeding with default values..")
           //  throw err;
     }
     else {
-        if ('SecretString' in data) {
-            secret = JSON.parse(data.SecretString);
+        if ("SecretString" in data) {
+            const secret = JSON.parse(data.SecretString)
             for(const envKey of Object.keys(secret)) {
-                process.env[envKey] = secret[envKey];
-              //  console.log(` Value for key '${envKey}' `);
-              //  console.log(` secret[envKey] '${secret[envKey]}'`);
-                if (envKey == 'user') {
+                process.env[envKey] = secret[envKey]
+              //  console.log(` Value for key "${envKey}" `);
+              //  console.log(` secret[envKey] "${secret[envKey]}"`);
+                if (envKey == "user") {
                   config.APP_DB_USER = secret[envKey]
-                } else if (envKey == 'password') {
+                } else if (envKey == "password") {
                   config.APP_DB_PASSWORD = secret[envKey]
-                } else if (envKey == 'host') {
+                } else if (envKey == "host") {
                   config.APP_DB_HOST = secret[envKey]
-                } else if (envKey == 'db') {
+                } else if (envKey == "db") {
                   config.APP_DB_NAME= secret[envKey]
                 }
         }
@@ -47,15 +47,15 @@ client.getSecretValue({SecretId: secretName}, function(err, data) {
     }
     // console log in case of error
     //console.log(err);
-});
+})
 
 
 Object.keys(config).forEach(key => {
   if(process.env[key] === undefined){
-    console.log(`[NOTICE] Value for key '${key}' not found in ENV, using default value.  See app/config/config.js`)
+    console.log(`[NOTICE] Value for key "${key}" not found in ENV, using default value.  See app/config/config.js`)
   } else {
     config[key] = process.env[key]
   }
-});
+})
 
-module.exports = config;
+module.exports = config
--- phase 5-6/web/index.js
+++ phase 5-6/web/index.js
@@ -1,48 +1,48 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require("express")
+const bodyParser = require("body-parser")
 const cors = require("cors")
-const supplier = require("./app/controller/supplier.controller");
-const app = express();
+const supplier = require("./app/controller/supplier.controller")
+const app = express()
 const mustacheExpress = require("mustache-express")
-const favicon = require('serve-favicon');
+const favicon = require("serve-favicon")
 
 // parse requests of content-type: application/json
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 // parse requests of content-type: application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cors());
-app.options("*", cors());
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
+app.options("*", cors())
 app.engine("html", mustacheExpress())
 app.set("view engine", "html")
 app.set("views", __dirname + "/views")
-app.use(express.static('public'));
-app.use(favicon(__dirname + "/public/img/favicon.ico"));
+app.use(express.static("public"))
+app.use(favicon(__dirname + "/public/img/favicon.ico"))
 
 // list all the students
 app.get("/", (req, res) => {
-    res.render("home", {});
-});
-app.get("/students/", supplier.findAll);
+    res.render("home", {})
+})
+app.get("/students/", supplier.findAll)
 // show the add suppler form
 app.get("/supplier-add", (req, res) => {
-    res.render("supplier-add", {});
-});
+    res.render("supplier-add", {})
+})
 // receive the add supplier POST
-app.post("/supplier-add", supplier.create);
+app.post("/supplier-add", supplier.create)
 // show the update form
-app.get("/supplier-update/:id", supplier.findOne);
+app.get("/supplier-update/:id", supplier.findOne)
 // receive the update POST
-app.post("/supplier-update", supplier.update);
+app.post("/supplier-update", supplier.update)
 // receive the POST to delete a supplier
-app.post("/supplier-remove/:id", supplier.remove);
+app.post("/supplier-remove/:id", supplier.remove)
 // handle 404
-app.use(function (req, res, next) {
-    res.status(404).render("404", {});
+app.use(function (req, res) {
+    res.status(404).render("404", {})
 })
 
 
 // set port, listen for requests
-const app_port = process.env.APP_PORT ||3000
+const app_port = process.env.APP_PORT || 3000
 app.listen(app_port, () => {
-    console.log(`Server is running on port ${app_port}.`);
-});
+    console.log(`Server is running on port ${app_port}.`)
+})
